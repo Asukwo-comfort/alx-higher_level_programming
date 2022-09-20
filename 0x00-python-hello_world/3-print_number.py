c3136d8feb315a2a507a3bd182f4b647cbd01f01
asukwo-comfort <asukwokulex24@gmail.com>
2022-09-19 19:49:34 -0700

    modified

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 number = 98
-<<<<<<< HEAD
-print("{:d} Battery street".format(number))
-=======
 print(f"{number:d} Battery street")
->>>>>>> f1891a1fa7f03c694d3976c6e7f4992b4419360d
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-<<<<<<< HEAD
 print("Float: {:.2f}".format(number))
-=======
-print(f"Float: {number:.2f}")
->>>>>>> f1891a1fa7f03c694d3976c6e7f4992b4419360d
