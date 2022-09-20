fcfd1988f22d6fc08e8bb89a8710292a3ca30fd0
asukwo-comfort <asukwokulex24@gmail.com>
2022-09-19 20:12:31 -0700

    modified

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,10 +5,7 @@
  * @list: linked list to check
  * Return: 1 (linked list is circular) 0 (no loop detected)
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> f1891a1fa7f03c694d3976c6e7f4992b4419360d
 int check_cycle(listint_t *list)
 {
 	listint_t *z1 = NULL, *z2 = NULL;
@@ -21,11 +18,7 @@ int check_cycle(listint_t *list)
 		if (!z2 || !z1)
 			return (0);
 		if (z2->next == z1)
-<<<<<<< HEAD
 			return (1);
-=======
-			return (1); 
->>>>>>> f1891a1fa7f03c694d3976c6e7f4992b4419360d
 	}
 	return (0);
 }
