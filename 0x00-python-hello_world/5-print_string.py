faf2943899b8c1fcc8c50597f4304e7b4f38ffbc
asukwo-comfort <asukwokulex24@gmail.com>
2022-09-19 19:57:30 -0700

    modified

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-<<<<<<< HEAD
-print("{}".format(str) * 3)
+print(str*3)
 print(str[:9])
-=======
-print(str+str+str+'\n'+str[:9])
->>>>>>> f1891a1fa7f03c694d3976c6e7f4992b4419360d
